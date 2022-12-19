711e8f493c984e3c56680e6839a2b9e9fc19cac4
melton-paul <melton.paul@hotmail.com>
2022-12-19 16:45:43 -0600

    Privacy agreement structure

--- src/components/Privacy/Privacy.tsx
+++ src/components/Privacy/Privacy.tsx
@@ -4,66 +4,90 @@ const Privacy = () => {
   return (
     <section className={`${styles.privacy} container`}>
       <h2 className={styles["privacy-title"]}>Privacy Agreement</h2>
-      <p>
+      <p className={styles["privacy-body"]}>
         This Privacy Statement applies to the www.golorewards.com website and
         associated websites. GoLoRewards understands that you are concerned
         about your privacy. That's why GoLoRewards has created this privacy
         statement, to demonstrate our firm commitment to your privacy.
-        <br />
-        <br />
-        How does GoLoRewards use the information I provide? <br />
-        <br />
-        GoLoRewards uses the information you provide to contact you or to send
-        you information and announcements concerning GoLoRewards services and
-        websites. GoLoRewards will not sell or give away your personal
-        information to any other person, company or organisation, unless it is
-        required by law, or you expressly agree to us giving out your
-        information.
-        <br />
-        <br /> What about security?
-        <br />
-        <br />
-        GoLoRewards has modern and robust security measures in place to protect
-        the information you give to us from loss, misuse and alteration. We are
-        constantly reviewing and upgrading these security measures to ensure the
-        very best of protection to you.
-        <br />
-        <br /> What are cookies and why does GoLoRewards use them?
-        <br />
-        <br /> A cookie is a piece of data stored on a user's computer hard
-        drive, containing information about that user. A cookie can only be
-        accessed across the Internet by the website that placed it there.
-        Cookies are used by the GoLoRewards website to add functionality and
-        security. You may choose to reject a cookie that the GoLoRewards website
-        attempts to place on your computer, however you may then be limited in
-        your use of some areas of the website. The use of cookies in this way is
-        wide-spread and commonly accepted by the Internet community.
-        <br />
-        <br />
-        What about other websites?
-        <br />
-        <br /> The GoLoRewards website contains links to other websites. We are
-        not responsible for the privacy practices of these other websites. We
-        encourage you to be aware when you leave our website and to read the
-        privacy statements of other websites that you use. This privacy
-        statement applies only to this website.
-        <br />
-        <br /> What is "spam" and does GoLoRewards send it?
-        <br />
-        <br /> The widely accepted definition of "spam" is unsolicited
-        commercial email, or in other words, email of a commercial nature that
-        the recipient does not want. GoLoRewards does not send "spam".
-        <br />
-        <br /> How can I stop GoLoRewards from sending me email?
-        <br />
-        <br /> Each email we send to you contains a link at the very bottom
-        allowing you to unsubscribe. If you have received unwanted email sent
-        from GoLoRewards, or purporting to be sent from GoLoRewards, please
-        contact GoLoRewards. If you have any questions about the privacy
-        practices of GoLoRewards or your dealings with GoLoRewards, please
-        contact GoLoRewards. We are committed to being of service to you and
-        resolving any issues that may arise.
       </p>
+      <ul className={styles["privacy-list"]}>
+        <li>
+          <h3 className={styles["privacy-list--title"]}>
+            How does GoLoRewards use the information I provide?
+          </h3>
+          <p>
+            GoLoRewards uses the information you provide to contact you or to
+            send you information and announcements concerning GoLoRewards
+            services and websites. GoLoRewards will not sell or give away your
+            personal information to any other person, company or organisation,
+            unless it is required by law, or you expressly agree to us giving
+            out your information.
+          </p>
+        </li>
+        <li>
+          <h3 className={styles["privacy-list--title"]}>
+            What about security?
+          </h3>
+          <p>
+            GoLoRewards has modern and robust security measures in place to
+            protect the information you give to us from loss, misuse and
+            alteration. We are constantly reviewing and upgrading these security
+            measures to ensure the very best of protection to you.
+          </p>
+        </li>
+        <li>
+          <h3 className={styles["privacy-list--title"]}>
+            What are cookies and why does GoLoRewards use them?
+          </h3>
+          <p>
+            A cookie is a piece of data stored on a user's computer hard drive,
+            containing information about that user. A cookie can only be
+            accessed across the Internet by the website that placed it there.
+            Cookies are used by the GoLoRewards website to add functionality and
+            security. You may choose to reject a cookie that the GoLoRewards
+            website attempts to place on your computer, however you may then be
+            limited in your use of some areas of the website. The use of cookies
+            in this way is wide-spread and commonly accepted by the Internet
+            community.
+          </p>
+        </li>
+        <li>
+          <h3 className={styles["privacy-list--title"]}>
+            What about other websites?
+          </h3>
+          <p>
+            The GoLoRewards website contains links to other websites. We are not
+            responsible for the privacy practices of these other websites. We
+            encourage you to be aware when you leave our website and to read the
+            privacy statements of other websites that you use. This privacy
+            statement applies only to this website.
+          </p>
+        </li>
+        <li>
+          <h3 className={styles["privacy-list--title"]}>
+            What is "spam" and does GoLoRewards send it?
+          </h3>
+          <p>
+            The widely accepted definition of "spam" is unsolicited commercial
+            email, or in other words, email of a commercial nature that the
+            recipient does not want. GoLoRewards does not send "spam".
+          </p>
+        </li>
+        <li>
+          <h3 className={styles["privacy-list--title"]}>
+            How can I stop GoLoRewards from sending me email?
+          </h3>
+          <p>
+            Each email we send to you contains a link at the very bottom
+            allowing you to unsubscribe. If you have received unwanted email
+            sent from GoLoRewards, or purporting to be sent from GoLoRewards,
+            please contact GoLoRewards. If you have any questions about the
+            privacy practices of GoLoRewards or your dealings with GoLoRewards,
+            please contact GoLoRewards. We are committed to being of service to
+            you and resolving any issues that may arise.
+          </p>
+        </li>
+      </ul>
     </section>
   );
 };
