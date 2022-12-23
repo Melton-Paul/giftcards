c7751c528665149f6cb58e4c7624bbbd2f1fdd08
melton-paul <melton.paul@hotmail.com>
2022-12-23 17:09:47 -0600

    Seperated routes/pages, added privacy routing

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,6 @@ import { useContext } from "react";
 import Logo from "../Logo/Logo";
 import infoStore from "../../store/info-store";
 
-const Footer = () => {
   const infoCtx = useContext(infoStore);
 
   return (
