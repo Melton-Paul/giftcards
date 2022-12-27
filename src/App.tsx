ab972d00ec79e97d7bc8a56334aceab45d2e3841
melton-paul <melton.paul@hotmail.com>
2022-12-27 15:56:28 -0600

    Added google tag manager

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,17 @@ import CustomerRoutes from "./routes/CustomerRoutes";
 import infoStore from "./store/info-store";
 import { Routes, Route } from "react-router";
 import Privacy from "./components/Privacy/Privacy";
+import TagManager from "react-gtm-module";
 
 function App() {
   const infoCtx = React.useContext(infoStore);
   const [selected, setIsSelected] = React.useState(false);
+
+  const tagManagerArgs = {
+    gtmId: "GTM-TF9M4TG",
+  };
+
+  TagManager.initialize(tagManagerArgs);
 
   return (
     <div className="app">
